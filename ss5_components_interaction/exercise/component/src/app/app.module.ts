0b4f30afeb1806a2b010a647dd1435fd529a910b
quynhtran11061999 <quynhtran11061999@gmail.com>
2022-09-29 08:51:01 +0700

    up

---
+++ ss5_components_interaction/exercise/component/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RatingBarComponentComponent } from './rating-bar-component/rating-bar-component.component';
+import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RatingBarComponentComponent,
+    CountdownTimerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
