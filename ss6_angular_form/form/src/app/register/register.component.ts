db0b50eb515b55b272013d493894b73a1aa22f0d
quynhtran11061999 <quynhtran11061999@gmail.com>
2022-09-29 17:18:18 +0700

    up

--- ss6_angular_form/form/src/app/register/register.component.ts
+++ ss6_angular_form/form/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -8,13 +8,31 @@ import {FormGroup} from "@angular/forms";
 })
 export class RegisterComponent implements OnInit {
   registerFrom: FormGroup;
+  submitter= false;
+
   constructor() {
     this.registerFrom = new FormGroup({
+    email: new FormControl("",[Validators.required,Validators.pattern("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")]),
+    password: new FormControl("",[Validators.required,Validators.minLength(6)]),
+    confirmPassword: new FormControl("",[Validators.required,Validators.minLength(6)]),
+    country: new FormControl("",[Validators.required]),
+    age: new FormControl("",[Validators.required,Validators.min(18)]),
+    gender: new FormControl("",[Validators.required]),
+    phone: new FormControl("",[Validators.required,Validators.pattern("/^\\+84\\d{9,10}$/")])
+  }, {validators: this.checkPasswords});
+  }
 
-    })
+  checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => {
+    let pass = group.get('password').value;
+    let confirmPass = group.get('confirmPassword').value
+    return pass === confirmPass ? null : { check: true }
   }
 
   ngOnInit(): void {
   }
 
+  createRegister() {
+    this.submitter = true
+    console.log(this.registerFrom)
+  }
 }
--- ss6_angular_form/form/src/model/i-person.ts
+++ ss6_angular_form/form/src/model/i-person.ts
@@ -1,3 +1,9 @@
 export interface IPerson {
-  email
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+  country?: string;
+  age?: number;
+  gender?: boolean;
+  phone?: string;
 }
